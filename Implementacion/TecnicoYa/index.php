76e93cbce0e0dd00a816b992b41a1846380c979b
MRodriguez08 <mau.rod.81090@gmail.com>
2013-10-16 22:11:19 -0200

    Framework funcionando

--- Implementacion/TecnicoYa/index.php
+++ Implementacion/TecnicoYa/index.php
@@ -1,12 +1,12 @@
 <?php
-
+	
 	/*** error reporting on ***/
 	error_reporting(E_ALL);
 
 	/*** define the site path constant ***/
 	$site_path = realpath(dirname(__FILE__));
 	define ('__SITE_PATH', $site_path);
-
+	
 	/*** include the init.php file ***/
 	include 'includes/init.php';
 	
@@ -14,6 +14,12 @@
 	$registry->router = new router($registry);
 	
 	/*** set the path to the controllers directory ***/
-	$router->setPath (__SITE_PATH . 'controller');
+	$registry->router->setPath (__SITE_PATH . '/controller');
+	
+	 /*** load up the template ***/
+	$registry->template = new template($registry);
+	
+	/*** load the controller ***/
+	$registry->router->loader();
 
 ?>
